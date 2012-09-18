f5d8afffcc6138fb87916ecedc26ba9f9f6677a1
qingyue <qingyue@onyx-international.com>
2012-09-18 19:05:19 +0800

    add note

--- android/src/org/coolreader/CoolReader.java
+++ android/src/org/coolreader/CoolReader.java
@@ -2273,6 +2273,7 @@ public class CoolReader extends Activity
     	Log.i(LOG_TEXT_KEY, activity);
     }
 
+    //Set fullscreen
     @Override
     public void onWindowFocusChanged(boolean hasFocus)
     {
