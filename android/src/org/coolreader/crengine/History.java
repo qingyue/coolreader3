d62558d42d2aba9f4b9db4b41a8091d384e279b3
qingyue <qingyue@onyx-international.com>
2012-11-19 10:58:56 +0800

    add ImageDataCache

--- android/src/org/coolreader/crengine/History.java
+++ android/src/org/coolreader/crengine/History.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.coolreader.CoolReader;
 import org.coolreader.db.CRDBService;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 
 public class History extends FileInfoChangeSource {
@@ -170,4 +171,87 @@ public class History extends FileInfoChangeSource {
 		return true;
 	}
 
+	static class ImageData {
+        long bookId;
+        byte[] data;
+        BitmapDrawable drawable = null;
+    }
+
+	class ImageDataCache {
+        private final int maxSize;
+        private int dataSize = 0;
+        private int maxCount = 15;
+        private ArrayList<ImageData> list = new ArrayList<ImageData>();
+        public ImageDataCache( int maxSize, int maxCount ) {
+            this.maxSize = maxSize;
+            this.maxCount = maxCount;
+        }
+        synchronized public void clear() {
+            list.clear();
+        }
+        synchronized public byte[] get( long bookId ) {
+            for ( int i=0; i<list.size(); i++ )
+                if ( list.get(i).bookId==bookId )
+                    return list.get(i).data;
+            return null;
+        }
+        synchronized public void put( long bookId, byte[] data ) {
+            boolean found = false;
+            for ( int i=0; i<list.size(); i++ )
+                if ( list.get(i).bookId==bookId ) {
+                    dataSize -= list.get(i).data.length;
+                    dataSize += data.length;
+                    list.get(i).data = data;
+                    if ( i>0 ) {
+                        ImageData item = list.remove(i);
+                        list.add(0, item);
+                    }
+                    found = true;
+                    break;
+                }
+            if ( !found ) {
+                ImageData item = new ImageData();
+                item.bookId = bookId;
+                item.data = data;
+                list.add(0, item);
+                dataSize += data.length;
+            }
+            for ( int i=list.size()-1; i>0; i-- ) {
+                if ( dataSize>maxSize || list.size()>maxCount ) {
+                    ImageData item = list.remove(i);
+                    dataSize -= item.data.length;
+                } else
+                    break;
+            }
+        }
+        synchronized public BitmapDrawable getImage( long bookId )
+        {
+            ImageData item = null;
+            for ( int i=0; i<list.size(); i++ )
+                if ( list.get(i).bookId==bookId ) {
+                    item = list.get(i);
+                    break;
+                }
+            if ( item==null )
+                return null;
+            byte[] data = get(bookId);
+            if ( data==null || data.length==0 )
+                return null;
+            if ( item.drawable!=null )
+                return item.drawable;
+            // decode & resize
+            BitmapDrawable res = decodeCoverPage( data );
+            if ( res!=null ) {
+                item.drawable = res;
+            } else {
+                item.data = new byte[] {};
+            }
+            return res;
+        }
+        synchronized void invalidateImages()
+        {
+            for ( int i=0; i<list.size(); i++ )
+                list.get(i).drawable = null;
+        }
+    }
 }
